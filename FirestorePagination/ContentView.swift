feeeffd102e7673dd697b0b2940bf80017e037b0
SKPnation <ayomideseaz@gmail.com>
2021-01-08 19:21:56 +0100

    Initial Commit

---
+++ FirestorePagination/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirestorePagination
+//
+//  Created by Ayomide Ajayi on 08/01/2021.
+//  Copyright © 2021 Skiplab Innovation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
